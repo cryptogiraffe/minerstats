3890bc4bc231ad496856d025e23d90e535d25c8b
Eugene Bujak <hmage@hmage.net>
2016-04-28 13:07:00 +0000

    hashrate updates

--- minerstats.php
+++ minerstats.php
@@ -5,7 +5,7 @@ $gfxcards = array(
   'GPU 2x970' => array(
     "blake"       => 2978.05 * 1000, // ccminer_tpruvot_201604_64
     "blake2s"     => 2163.64 * 1000, // ccminer_tpruvot_201604_64
-    "decred"      => 2712.56 * 1000, // ccminer_tpruvot_201604_64
+    "decred"      => 3216.00 * 1000, // ccminer_alexis.x64 -i 29
     "ethereum"    =>   11.79 * 1000, // ethminer-0.9.41-genoil-1.0.6 -M 1266489
     "fresh"       =>   18.57 * 1000, // ccminer_sp_201603
     "groestl"     =>   46.62 * 1000, // ccminer_sp_201603
@@ -14,8 +14,8 @@ $gfxcards = array(
     "lyra2v2"     =>   19.25 * 1000, // ccminer_sp_201603
     "neoscrypt"   => 1244.97,        // ccminer_neoscrypt
     "nist5"       =>   55.76 * 1000, // ccminer_neoscrypt
-    "quark"       =>   34.60 * 1000, // ccminer_sp_1.5.79
-    "qubit"       =>   24.67 * 1000, // ccminer_sp_201603
+    "quark"       =>   33.80 * 1000, // ccminer_sp_1.5.79
+    "qubit"       =>   24.80 * 1000, // ccminer_sp_1.5.79 -i 21
     "scrypt"      => 1718.08,        // ccminer_sp_201603 -l T26x20
     "scrypt-jane" =>    0.893,       // ccminer_tpruvot -l t23x1
     "sha256"      => 1152.33 * 1000, // ccminer_sp_201603
@@ -24,12 +24,56 @@ $gfxcards = array(
     "skein2"      =>  347.44 * 1000, // ccminer_tpruvot
     "vanilla"     => 5190.86,        // ccminer_sp_201603
     "whirlpoolx"  =>  403.40 * 1000, // ccminer_neoscrypt
-    "x11"         =>   16.23 * 1000, // ccminer_neoscrypt
+    "x11"         =>   16.30 * 1000, // ccminer_neoscrypt
     "x13"         =>   12.83 * 1000, // ccminer_neoscrypt
     "x14"         =>   11.92 * 1000, // ccminer_tpruvot // neoscrypt and sp -- crash
     "x15"         =>   11.09 * 1000, // ccminer_neoscrypt
     "x17"         =>    9.78 * 1000, // ccminer_tpruvot // neoscrypt and sp -- crash
     "zr5"         => 6074.52,        // ccminer_tpruvot_201604_64
+  ),
+  'GPU 2x970 OC' => array( // +155Core +87mV 100%FAN 110%power
+    "blake"       => 3311.00 * 1000, // ccminer_tpruvot_1.7.5-git.x64
+    "blake2s"     => 2400.00 * 1000, // ccminer_tpruvot_1.7.5-git.x64 -i 26
+    "decred"      => 3572.00 * 1000, // ccminer_alexis.x64 -i 27
+    "fresh"       =>   20.55 * 1000, // ccminer_neoscrypt -i 21
+    "groestl"     =>   51.83 * 1000, // ccminer_so_1.5.79
+    "keccak"      => 1028.50 * 1000, // ccminer_neoscrypt -i 25
+    "lyra2"       => 4173.50,        // ccminer_sp_1.5.79
+    "lyra2v2"     =>   21.40 * 1000, // ccminer_sp_1.5.79 -i 19
+    "neoscrypt"   => 1382.24,        // ccminer_neoscrypt
+    "nist5"       =>   61.00 * 1000, // ccminer_neoscrypt
+    "quark"       =>   37.44 * 1000, // ccminer_sp_1.5.79 -i 23
+    "qubit"       =>   27.40 * 1000, // ccminer_sp_1.5.79 -i 21
+    "scrypt"      => 1718.08,        // ccminer_sp_1.5.79 -l T26x20
+    "scrypt-jane" =>  915.14 / 1000, // ccminer_tpruvot -l t23x1
+    "sha256"      => 1350.00 * 1000, // ccminer_sp_1.5.79
+    "sib"         => 3885.35,        // ccminer_tpruvot_1.7.5-git.x64
+    "skein"       =>  636.00 * 1000, // ccminer_sp_1.5.79
+    "skein2"      =>  384.34 * 1000, // ccminer_tpruvot_1.7.5-git.x64 -i 24
+    "vanilla"     => 5800.44,        // ccminer_sp_1.5.79
+    "whirlpoolx"  =>  440.40 * 1000, // ccminer_neoscrypt
+    "x11"         =>   18.00 * 1000, // ccminer_neoscrypt -i 21
+    "x13"         =>   14.23 * 1000, // ccminer_neoscrypt
+    "x14"         =>   12.71 * 1000, // ccminer_tpruvot_1.7.5-git.x64 -i 21 // neoscrypt and sp -- crash
+    "x15"         =>   12.35 * 1000, // ccminer_neoscrypt
+    "x17"         =>   10.83 * 1000, // ccminer_tpruvot_1.7.5-git.x64 // neoscrypt and sp -- crash
+    "zr5"         => 6646.00,        // ccminer_tpruvot_1.7.5-git.x64 -i 26
+  ),
+  'GPU 980Ti' => array(
+    "blake"       => 2427.28 * 1000, // nicehash default
+    "blakecoin"   => 4218.85 * 1000, // nicehash default
+    "keccak"      =>  661.21 * 1000, // nicehash default
+    "lyra2v2"     =>   15.57 * 1000, // nicehash default
+    "neoscrypt"   =>  922.00,        // nicehash default
+    "nist5"       =>   42.53 * 1000, // nicehash default
+    "quark"       =>   27.13 * 1000, // nicehash default
+    "qubit"       =>   20.12 * 1000, // nicehash default
+    "vanilla"     => 4238.21 * 1000, // nicehash default
+    "whirlpoolx"  =>  329.34 * 1000, // nicehash default
+    "x11"         =>   13.12 * 1000, // nicehash default
+    "x13"         =>   10.21 * 1000, // nicehash default
+    "x15"         =>    8.88 * 1000, // nicehash default
+    "ethereum"    =>   21.18 * 1000, // ethminer-0.9.41-genoil-1.0.6 -M 1294333
   ),
   'GPU 570' => array(
     "quark"       =>  527.30, // ccminer_oxen
@@ -50,6 +94,18 @@ $gfxcards = array(
     "x13"       => 3000, // sgminer-5-3-0-general
     "x15"       => 1320, // sgminer-5-3-0-general
   ),
+  'GPU 6970' => array(
+    "x11"       => 2000,
+    "x13" => 1600,
+    "keccak" => 209200,
+    "x15" => 1400,
+    "neoscrypt" => 39,
+    "qubit" => 2500,
+    "quark" => 2700,
+    "lyra2v2" => 1900,
+    "blake256r8" => 1443700,
+    "blake256r8vnl" => 1432700,
+  ),
 
   /********
    * CPU's
@@ -69,7 +125,7 @@ $gfxcards = array(
     "quark"       => 500.74,
     "qubit"       => 402.75,
     "scrypt"      =>  93.50,
-    "scrypt-jane" =>   0.210,
+    "scrypt-jane" =>   0.208,
     "sha256"      =>  50.84 * 1000,
     "vanilla"     =>  19.10 * 1000,
     "x11"         => 251.91,
@@ -77,6 +133,52 @@ $gfxcards = array(
     "x14"         => 159.72,
     "x15"         => 152.57,
     "yescrypt"    =>   1.61,
+  ),
+  'CPU i7 4770 OC' => array( // 104x39 -- 4056MHz vs stock 3900MHz
+    "anime"       => 607877 / 1000,
+    "argon2"      => 29551 / 1000, // cpuminer-opt
+    "blake"       => 21100403 / 1000,
+    "blake2s"     => 17088083 / 1000,
+    "blakecoin"   => 29765247 / 1000,
+    "bmw"         => 9741833 / 1000,
+    "c11"         => 706.74, // cpuminer-opt
+    "cryptonight" => 200 / 1000, // cpuminer-opt
+    "decred"      => 21053593 / 1000, // cpuminer-opt
+    "dmd-gr"      => 1121266 / 1000,
+    "drop"        => 30984 / 1000,
+    "fresh"       => 506625 / 1000,
+    "groestl"     => 1118556 / 1000,
+    "keccak"      => 8402648 / 1000,
+    "luffa"       => 3161042 / 1000,
+    "lyra2"       => 936413 / 1000,
+    "lyra2v2"     => 466049 / 1000,
+    "myr-gr"      => 3394.48, // cpuminer-opt
+    "neoscrypt"   => 28747 / 1000,
+    "nist5"       => 1885.70, // cpuminer-opt
+    "pentablake"  => 3708888 / 1000,
+    "pluck"       => 1838 / 1000,
+    "quark"       => 1100.29, // cpuminer-opt
+    "qubit"       => 1033.30, // cpuminer-opt
+    "s3"          => 1222257 / 1000,
+    "scrypt"      => 100370 / 1000,
+    "scrypt-jane" => 226 / 1000,
+    "sha256d"     => 55760032 / 1000,
+    "shavite3"    => 2385939 / 1000,
+    "sib"         => 487.05, // cpuminer-opt
+    "skein"       => 6646851 / 1000,
+    "skein2"      => 8396332 / 1000,
+    "x11"         => 704586 / 1000, // cpuminer-opt
+    "x13"         => 276196 / 1000, // cpuminer-opt
+    "x14"         => 318383 / 1000, // cpuminer-opt
+    "x15"         => 293850 / 1000, // cpuminer-opt
+    "x17"         => 279510 / 1000, // cpuminer-opt
+    "yescrypt"    => 3194 / 1000, // cpuminer-opt
+    "zr5"         => 795.73, // cpuminer-opt
+  ),
+  'CPU i7 2600K' => array(
+    "lyra2"       => 485.850, // nicehash default
+    "axiom"       =>   0.139, // nicehash default
+    "scrypt-jane" =>   0.184, // nicehash default
   ),
   'CPU i7 6700' => array(
     "axiom"       =>   0.399,
@@ -88,9 +190,18 @@ $gfxcards = array(
     "lyra2"       => 397.56,
     "scrypt-jane" =>   0.056,
   ),
+  'CPU i5 4690k' => array(
+    "axiom" => 0.275,
+    "lyra2" => 678,
+    "scrypt-jane" => 0.131,
+  ),
+  /*
   'VPS AWS t2.micro' => array(
     "lyra2"       =>  13.95,
     "scrypt-jane" =>   2.130 / 1000,
+  ),
+  'VPS scaleway $3/mo' => array(
+    "lyra2"       =>  92.58,
   ),
   'VPS vultr $5/mo' => array(
     "anime"         => 98780 / 1000,
@@ -128,6 +239,7 @@ $gfxcards = array(
     "x15"           => 26121 / 1000,
     "zr5"           => 70582 / 1000,
   ),
+  */
 );
 ?>
 <!DOCTYPE html>
